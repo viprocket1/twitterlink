23fdc05d580be1057bf08f37af895cd0e648af51
vipul charthal <47239352+viprocket1@users.noreply.github.com>
2021-07-19 18:43:08 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="Para">
+        <text>
+          Hey, My name is Vipul. I published my first computer program at the
+          age of 16 and two years later, after an intensive studies in math,
+          physics and chemistry, I was ranked amongst the highest candidates in
+          the academic examination conducted by Joint Admission Board, JEE
+          Advanced. I graduated from Indian Institute of Petroleum and Energy. I
+          was selected for Internship program at Amazon. were I worked on to
+          enhancing my technical skills in the areas of Artificial Intelligence
+          and Web technologies. After Graduation, I refused to follow the
+          traditional career path and started working on solving and finding
+          better problems to challenge and put my skills to use.
+        </text>
+      </div>
+      <div>
+        <a href={"https://twitter.com/reddit69"}>twitter-</a>
+
+        <a href={"https://github.com/viprocket1"}>-github-</a>
+        <a href={"https://www.linkedin.com/in/vipul-sanjay-282318128/"}>
+          -linkedin-
+        </a>
+      </div>
+      <div>
+        <text style={{ fontSize: 10 }}>i don't have a instagram</text>
+      </div>
+    </div>
+  );
+}
